3abe3c584f338105c0d7f62c402403c4a62ad076
Vasko <magaranov@abv.bg>
2023-12-22 15:52:58 +0200

    guid generator tool

---
+++ CSharp/QuickGUIDGenerator/QuickGUIDGenerator/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Forms;
+
+public class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        Guid guid = Guid.NewGuid();
+        Clipboard.SetText(guid.ToString().ToUpper());
+    }
+}
