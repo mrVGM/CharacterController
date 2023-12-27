7043d6ed46a870d395886eaa4e17656906c3ada1
Vasko <magaranov@abv.bg>
2023-12-27 21:14:44 +0200

    made the unlit material looks like checkers board

--- Data/Shaders/src/unlitPS.hlsl
+++ Data/Shaders/src/unlitPS.hlsl
@@ -1,4 +1,16 @@
-float4 PSMain(float4 position : SV_POSITION, float4 worldPosition : WORLD_POSITION, float4 normal : NORMAL) : SV_Target
+float4 PSMain(
+    float4 position : SV_POSITION,
+    float4 worldPosition : WORLD_POSITION,
+    float4 normal : NORMAL,
+    float2 uv : UV) : SV_Target
 {
-    return float4(1, 1, 0, 1);
+    float4 color1 = float4(1, 1, 0, 1);
+    float4 color2 = float4(0.5, 1, 0, 1);
+    
+    float2 tmp = uv / 0.01;
+    int2 index = floor(tmp);
+    
+    float4 color = (index.x + index.y) % 2 == 0 ? color1 : color2;
+    
+    return color;
 }
