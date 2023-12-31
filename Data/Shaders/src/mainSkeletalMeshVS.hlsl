964f9775b4bca13171406a668ebaaaeab1fc5cc5
Vasko <magaranov@abv.bg>
2023-12-31 18:14:10 +0200

    skeletal mesh vertes shader HLSL

---
+++ Data/Shaders/src/mainSkeletalMeshVS.hlsl
@@ -0,0 +1,33 @@
+#include "common_buffers_lib.hlsl"
+
+cbuffer CamBuff : register(b0)
+{
+    CameraBuffer m_camBuff;
+};
+
+StructuredBuffer<int> v_weights     : register(t0);
+StructuredBuffer<float4x4> v_pose   : register(t1);
+
+struct PSInput
+{
+    float4 position         : SV_POSITION;
+    float4 world_position   : WORLD_POSITION;
+    float4 normal           : NORMAL;
+    float2 uv               : UV;
+};
+
+PSInput VSMain(VertexInput3D vertexInput)
+{
+    PSInput result;
+
+    float3 worldPos = vertexInput.position;
+    float3 worldNormal = vertexInput.normal;
+
+    result.position = mul(m_camBuff.m_matrix, float4(worldPos, 1));
+
+    result.world_position = float4(worldPos, 1);
+    result.normal = float4(worldNormal, 1);
+    result.uv = vertexInput.uv;
+
+    return result;
+}
